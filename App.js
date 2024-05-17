ee53f05d50ae71b3ad3aade974e7ef736d80add4
Smouzen <tlsmous09@yahoo.com>
2024-05-17 12:23:55 +0200

    fixed the database connection

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { MongoClient } from "mongodb";
 
-const password = encodeURIComponent(process.env.MONGO_PASSWORD);
+const password = encodeURIComponent(process.env.MONGO_PASSWORD?.trim());
 const connectionString = `mongodb+srv://lsmous27:${password}@devcluster.wl59cem.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
-} catch(e) {
+  console.log("connection successful");
+} catch (e) {
   console.error(e);
 }
-let db = conn.db("smouzen_people");
+let db = conn?.db("smouzen_people");
 export default db;
